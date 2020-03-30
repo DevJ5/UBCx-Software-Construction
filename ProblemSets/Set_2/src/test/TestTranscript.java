c856ad065bb3f1c83c4b1b64a4a4da048a0bb413
DevJ5 <idevelopjs@gmail.com>
2020-03-30 13:38:05 +0200

    Transcrip tests

--- ProblemSets/Set_2/src/test/TestTranscript.java
+++ ProblemSets/Set_2/src/test/TestTranscript.java
@@ -2,29 +2,98 @@ package test;
 
 import model.Transcript;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestTranscript {
-    Transcript testTranscript;
+
+    private Transcript transcript;
+    private static final float TOL = 1.000f;
 
     @BeforeEach
-    public void setUp() {
-        this.testTranscript = new Transcript("John Doe", 123);
+    public void setUp(){
+        transcript = new Transcript("Jane Doe", 7001);
     }
 
     @Test
-    public void testConstructor() {
-        assertEquals("John Doe", testTranscript.getStudentName());
-        assertEquals(123, testTranscript.getStudentID());
-
+    public void testConstructor(){
+        assertEquals(transcript.getStudentName(), "Jane Doe");
+        assertEquals(transcript.getStudentID(), 7001);
+        assertEquals(transcript.getCourseNames().size(),0);
+        assertEquals(transcript.getGrades().size(),0);
     }
 
     @Test
-    public void testTwo() {
-        assertEquals(1,3,1);
-
+    public void testSetters(){
+        transcript.setStudentID(4000);
+        transcript.setStudentName("John Smith");
+        assertEquals(transcript.getStudentName(), "John Smith");
+        assertEquals(4000, transcript.getStudentID());
     }
+
+    @Test
+    public void testAddGrade(){
+        transcript.addGrade("BUSI 300", 3.9);
+        assertEquals(transcript.getCourseNames().size(),
+                transcript.getGrades().size(), 1);
+
+        loadGrades();
+        assertEquals(transcript.getCourseNames().size(), transcript.getGrades().size());
+    }
+
+
+    @Test
+    public void testGetCourseAndGrade(){
+        loadGrades();
+        assertEquals(transcript.getCourseAndGrade("PHYS 203"), "PHYS 203: 2.5");
+        assertEquals(transcript.getCourseAndGrade("ENGL 100"), "ENGL 100: 3.2");
+    }
+
+    @Test
+    public void testGetGPA(){
+        loadGrades();
+        double avg = ((2.5 + 3.2) / 2);
+        assertEquals(avg, transcript.getGPA(), TOL);
+
+        transcript.addGrade("JPNS 105", 3.8);
+        avg = ((2.5 + 3.2 + 3.8) / 3);
+        assertEquals(avg, transcript.getGPA(), TOL);
+    }
+
+    // 'tests' printTranscript() in main
+
+    private void loadGrades(){
+        transcript.addGrade("ENGL 100", 3.2);
+        transcript.addGrade("PHYS 203", 2.5);
+    }
+
+
+    //tests for extra problems:
+
+    @Test
+    public void testGetGradeByCourse(){
+        loadGrades();
+        assertEquals(transcript.getGradeByCourse("ENGL 100"), 3.2, TOL);
+        assertEquals(transcript.getGradeByCourse("PHYS 203"), 2.5, TOL);
+    }
+
+    @Test
+    public void testGetAverageOverSelectedCourses(){
+        loadGrades();
+        transcript.addGrade("SOCI 200", 1.5);
+
+        List<String> selectedCourses = new ArrayList<>();
+        selectedCourses.add("SOCI 200");
+        selectedCourses.add("ENGL 100");
+
+        double avg = (1.5 + 3.2) / 2;
+
+        assertEquals(transcript.getAverageOverSelectedCourses(selectedCourses), avg, TOL);
+    }
+
+
 }
